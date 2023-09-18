f7a0a116bcc96c1aaaf078cf1beac1de91f79321
Henrique Siconeli Francisco <henrique.siconeli.f@hotmail.com>
2023-09-18 12:48:53 -0400

    update

--- processo/views.py
+++ processo/views.py
@@ -4,13 +4,12 @@ from django.views.generic.edit import CreateView
 
 from django.urls import reverse, reverse_lazy
 
-
-
 ###### CREATE ######
 class ProcessoAdmCreate(CreateView):
     model = ProcessoAdm
     template_name = 'processo/processo_adm_create.html'
     fields = ['numero', 'municipio', 'uf', 'data_inicial', 'data_final', 'data_div_ativa', 'valor_atributo', 'valor_multa', 'valor_credito', 'valor_atualizado', 'data_valor_atualizado', 'nome_contribuinte', 'tipo_pessoa', 'documento', 'nome_fantasia', 'email', 'endereco', 'complemento', 'municipio_contribuinte', 'uf_contribuinte', 'cep', 'telefone', 'celular']
+    exclude = ['criador_processo_adm',]
     success_url = reverse_lazy('index')
 
 
