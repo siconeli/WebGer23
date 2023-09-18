d7a033c80957c14dfb9fe82099b233126e75bc14
Henrique Siconeli Francisco <henrique.siconeli.f@hotmail.com>
2023-09-18 16:02:45 -0400

    update

--- processo/urls.py
+++ processo/urls.py
@@ -3,13 +3,13 @@ from django.urls import path
 from .views import ProcessoAdmCreate
 # from .views import AndamentoAdmCreate
 
-# from .views import ProcessoAdmUpdate
+from .views import ProcessoAdmUpdate
 # from .views import AndamentoAdmUpdate
 
 # from .views import ProcessoAdmDelete
 # from .views import AndamentoAdmDelete
 
-# from .views import ProcessoAdmList
+from .views import ProcessoAdmList
 # from .views import AndamentoAdmList
 # from .views import ArquivoAdmList
 
@@ -17,5 +17,11 @@ urlpatterns = [
 
     ###### CREATE ######
     path('cadastrar/processo-adm/', ProcessoAdmCreate.as_view(), name='proc-adm-create'), 
+
+    ###### UPDATE ######
+    path('editar/processo-adm/', ProcessoAdmUpdate.as_view(), name='proc-adm-update'),
+
+    ###### LIST ######
+    path('listar/processo-adm/', ProcessoAdmList.as_view(), name='proc-adm-list'),
 
 ]
--- processo/views.py
+++ processo/views.py
@@ -1,6 +1,8 @@
 from .models import ProcessoAdm
 
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView # Módulo para criar, atualizar e deletar
+
+from django.views.generic.list import ListView # Módulo para listar
 
 from django.urls import reverse, reverse_lazy
 
@@ -11,9 +13,18 @@ class ProcessoAdmCreate(CreateView):
     fields = ['numero', 'municipio', 'uf', 'data_inicial', 'data_final', 'data_div_ativa', 'valor_atributo', 'valor_multa', 'valor_credito', 'valor_atualizado', 'data_valor_atualizado', 'nome_contribuinte', 'tipo_pessoa', 'documento', 'nome_fantasia', 'email', 'endereco', 'complemento', 'municipio_contribuinte', 'uf_contribuinte', 'cep', 'telefone', 'celular']
     success_url = reverse_lazy('index')
     
-    # Função para preencher o atributo 'criador_processo_adm' com o ID do usuário logado
+    # Função para preencher o atributo 'criador_processo_adm' com o ID do usuário logado antes do formulário ser salvo.
     def form_valid(self, form):
         form.instance.criador_processo_adm = self.request.user
-        return super().form_valid(form)
-    
-    
+        return super().form_valid(form)    
+
+###### UPDATE ######
+class ProcessoAdmUpdate(UpdateView):
+    model = ProcessoAdm
+    template_name = 'processo/processo_adm_update.html'
+    fields = ['numero', 'municipio', 'uf', 'data_inicial', 'data_final', 'data_div_ativa', 'valor_atributo', 'valor_multa', 'valor_credito', 'valor_atualizado', 'data_valor_atualizado', 'nome_contribuinte', 'tipo_pessoa', 'documento', 'nome_fantasia', 'email', 'endereco', 'complemento', 'municipio_contribuinte', 'uf_contribuinte', 'cep', 'telefone', 'celular']
+
+###### LIST ######
+class ProcessoAdmList(ListView):
+    model = ProcessoAdm
+    template_name = 'processo/listas/processo_adm_list.html'
