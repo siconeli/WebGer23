fd014a157d8473ed0dad7ff9250cad0e6ff2c8bc
Henrique Siconeli Francisco <henrique.siconeli.f@hotmail.com>
2023-09-18 15:33:37 -0400

    update

--- processo/views.py
+++ processo/views.py
@@ -9,7 +9,11 @@ class ProcessoAdmCreate(CreateView):
     model = ProcessoAdm
     template_name = 'processo/processo_adm_create.html'
     fields = ['numero', 'municipio', 'uf', 'data_inicial', 'data_final', 'data_div_ativa', 'valor_atributo', 'valor_multa', 'valor_credito', 'valor_atualizado', 'data_valor_atualizado', 'nome_contribuinte', 'tipo_pessoa', 'documento', 'nome_fantasia', 'email', 'endereco', 'complemento', 'municipio_contribuinte', 'uf_contribuinte', 'cep', 'telefone', 'celular']
-    exclude = ['criador_processo_adm',]
     success_url = reverse_lazy('index')
-
-
+    
+    # Função para preencher o atributo 'criador_processo_adm' com o ID do usuário logado
+    def form_valid(self, form):
+        form.instance.criador_processo_adm = self.request.user
+        return super().form_valid(form)
+    
+    
