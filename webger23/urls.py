98473ed5c5b4a4edaf687d231eeb7b831e6f26a5
Henrique Siconeli Francisco <henrique.siconeli.f@hotmail.com>
2023-09-18 08:58:41 -0400

    update

--- webger23/urls.py
+++ webger23/urls.py
@@ -22,10 +22,12 @@ from django.conf.urls.static import static
 
 from django.conf import settings
 
+from django.views.generic.base import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('', include('processo.urls')),
+    path('contas/', include('django.contrib.auth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # Editar a Área Administrativa do Django
