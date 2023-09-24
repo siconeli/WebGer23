f5a3e679e1ab275ed8792c3b97cba332df99494e
siconeli <henrique.siconeli.f@hotmail.com>
2023-09-24 12:39:09 -0400

    update

--- processo/admin.py
+++ processo/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from .models import ProcessoAdm
+# from .models import ProcessoAdm
 
-@admin.register(ProcessoAdm)
-class ProcessoAdmAdmin(admin.ModelAdmin):
-    list_display = ('numero',)
+# @admin.register(ProcessoAdm) # Para mostrar meu modelo ProcessoAdm no Admin do Django
+# class ProcessoAdmAdmin(admin.ModelAdmin):
+#     list_display = ('numero',)
