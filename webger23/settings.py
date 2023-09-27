d1c7670ed2c6c35367014c8cd458b4756b59fea3
Henrique Siconeli Francisco <henrique.siconeli.f@hotmail.com>
2023-09-27 16:04:57 -0400

    update

--- webger23/settings.py
+++ webger23/settings.py
@@ -52,7 +52,8 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # 'django.middleware.clickjacking.XFrameOptionsMiddleware', # Comentei essa linha para que eu consigo abrir arquivos PDF em um iframe através do google chrome, pois o XframeOptions por segurança não permite, ao colocar o sistema em produção irei descomentar a linha para que a segurança se reestabeleça.
+
 ]
 
 ROOT_URLCONF = 'webger23.urls'
