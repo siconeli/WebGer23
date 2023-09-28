3eadb17c649848ae863ec801edccf98b6ea92018
Henrique Siconeli Francisco <henrique.siconeli.f@hotmail.com>
2023-09-28 16:15:44 -0400

    update

--- processo/conversor_arquivos.py
+++ processo/conversor_arquivos.py
@@ -2,16 +2,18 @@ import os
 
 from docx2pdf import convert # Módulo para converter .docx em pdf
 
+from time import sleep
+
 caminho = 'media/Arquivo'
 
 lista_arquivos = os.listdir(caminho) # Todos os arquivos dentro do caminho
 
 for arquivo in lista_arquivos:
     nome_arquivo = os.path.basename(arquivo) # Nome do arquivo
-    if nome_arquivo[-5:] == '.docx':
+    if nome_arquivo[-5:] == '.docx':  # Se arquivo possuir formato .docx ira converter para pdf
         convert(f'media/Arquivo/{nome_arquivo}')
-    else:
-        print('- Arquivo com outro formato, não foi possível converter')
+        sleep(10)
+        os.remove(f'media/Arquivo/{nome_arquivo}') # Remove o antigo arquivo com formato .docx   
     
 
 
