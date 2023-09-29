e075fcec7ffbfd2c8f4633f58512ba470c440fd9
Henrique Siconeli Francisco <henrique.siconeli.f@hotmail.com>
2023-09-29 08:54:14 -0400

    update

--- processo/views.py
+++ processo/views.py
@@ -1,5 +1,6 @@
-from typing import Any
-from django.db import models
+# from typing import Any
+# from django.db import models
+
 from .models import ProcessoAdm, AndamentoAdm
 
 from django.views.generic.edit import CreateView, UpdateView, DeleteView # Módulo para create, update e delete
@@ -15,8 +16,6 @@ import logging # Módulo para criar logs
 import datetime # Módulo para datas
 
 logger = logging.getLogger(__name__)
-
-
 
 import os # Módulo para trabalhar com pastas e arquivos
 
@@ -121,26 +120,31 @@ class AndamentoAdmCreate(CreateView):
         return context
 
     
-    # CÓDIGO PARA PEGAR O NOME DO ARQUIVO QUE É SALVO NO BANCO DE DADOS AO CRIAR O OBJETO ANDAMENTO E VINCULAR O ARQUIVO, PARA CONVERTER O ARQUIVO QUE É DO FORMATO .DOCX PARA O FORMATO .PDF E EXCLUIR O ANTIGO ARQUIVO, ALTERAR O NOME DO ARQUIVO SALVO NO BANCO DE DADOS.
-    andamentos = AndamentoAdm.objects.all()
+    def conversorPdf():
+        """
+            Converter arquivos com formato '.docx' para formato '.pdf', remover o antigo arquivo '.docx' e atualizar o nome do arquivo na coluna do banco de dados para que busque o novo arquivo corretamente. (Ass. Siconeli)
+        """
 
-    for andamento in andamentos:
-        caminho = 'media/Arquivo'
-        lista_arquivos = os.listdir(caminho) # Todos os arquivos dentro do caminho
-        for arquivo in lista_arquivos:
-            if arquivo[-5:] == '.docx':  # Se arquivo possuir formato .docx, ira converter para pdf
-                convert(f'media/Arquivo/{arquivo}')
-                # sleep(2)
-                os.remove(f'media/Arquivo/{arquivo}') # Remove o antigo arquivo com formato .docx   
+        andamentos = AndamentoAdm.objects.all()
 
-        nome_arquivo = andamento.arquivo.name # Nome do arquivo em 'str'
+        for andamento in andamentos:
+            caminho = 'media/Arquivo'
+            lista_arquivos = os.listdir(caminho) # Todos os arquivos dentro do caminho
+            for arquivo in lista_arquivos:
+                if arquivo[-5:] == '.docx':  # Se arquivo possuir formato .docx, ira converter para pdf
+                    convert(f'media/Arquivo/{arquivo}')
+                    # sleep(2)
+                    os.remove(f'media/Arquivo/{arquivo}') # Remove o antigo arquivo com formato .docx   
 
-        if nome_arquivo[-5:] == '.docx':
-            nome_convertido = f'{nome_arquivo[:-5]}.pdf'
-            andamento.arquivo.name = nome_convertido
-            andamento.save()
+            nome_arquivo = andamento.arquivo.name # Nome do arquivo em 'str'
+
+            if nome_arquivo[-5:] == '.docx':
+                nome_convertido = f'{nome_arquivo[:-5]}.pdf'
+                andamento.arquivo.name = nome_convertido
+                andamento.save()
+    
+    conversorPdf()
         
-    # FIM DO CÓDIGO
 
     
 
