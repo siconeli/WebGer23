f524512bf6ac5e7f2be4822e3993d1c440a18cfd
Henrique Siconeli Francisco <henrique.siconeli.f@hotmail.com>
2023-10-27 13:53:45 -0400

    update

--- webger23/wsgi.py
+++ webger23/wsgi.py
@@ -10,8 +10,10 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling, MediaCling
+# from dj_static import Cling, MediaCling # Usado durante a Produção.
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webger23.settings')
 
-application = Cling(MediaCling(get_wsgi_application()))
+application = get_wsgi_application() # Usado durante o Desenvolvimento 
+
+# application = Cling(MediaCling(get_wsgi_application())) # Usado durante a Produção.  
