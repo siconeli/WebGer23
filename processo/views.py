6f1e53672bf15e90bc2b1d00718030aaf5952baa
Henrique Siconeli Francisco <henrique.siconeli.f@hotmail.com>
2023-10-27 15:50:26 -0400

    update

--- processo/views.py
+++ processo/views.py
@@ -37,6 +37,17 @@ class ProcessoAdmView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['dados_processo'] = ProcessoAdm.objects.filter(pk=processo_pk) # Filtra os dados do processo através da pk
         return context
+
+class ProcessoAdmArquivadoView(TemplateView):
+    template_name = 'processos/views/processo_adm_arquivado_view.html'
+
+    # Função para iterar com os dados do processo
+    def get_context_data(self, **kwargs):
+        processo_pk = self.kwargs.get('pk') # Pega a PK do processo através da URL  
+
+        context = super().get_context_data(**kwargs)
+        context['dados_processo'] = ProcessoAdm.objects.filter(pk=processo_pk) # Filtra os dados do processo através da pk
+        return context
     
 class AndamentoAdmView(TemplateView):
     template_name ='processos/views/andamento_adm_view.html'
@@ -324,6 +335,116 @@ class ProcessoAdmUpdate(UpdateView):
 
         except Exception as erro:
             logger.error(f'Erro ao editar objeto - View: ProcessoAdmUpdate - Erro: {str(erro)}')
+    
+class ProcessoAdmArquivadoUpdate(UpdateView):
+    model = ProcessoAdm
+    template_name = 'processos/updates/processo_adm_update.html'
+    fields = ['ativo', 'municipio', 'uf', 'data_inicial', 'data_final', 'data_div_ativa', 'valor_atributo', 'valor_multa', 'valor_credito', 'valor_atualizado', 'data_valor_atualizado', 'nome_contribuinte', 'tipo_pessoa', 'documento', 'nome_fantasia', 'email', 'endereco', 'complemento', 'municipio_contribuinte', 'uf_contribuinte', 'cep', 'telefone', 'celular']
+    success_url = reverse_lazy('proc-adm-arquivado-list')
+
+    # Função para iterar com os dados do processo na view de update processo
+    def get_context_data(self, **kwargs):
+        processo_pk = self.kwargs.get('pk') # Pega a PK do processo através da URL  
+
+        context = super().get_context_data(**kwargs)
+        context['dados_processo'] = ProcessoAdm.objects.filter(pk=processo_pk) # Filtra os dados do processo através da pk
+        return context
+    
+    def form_valid(self, form):  
+        """
+            O código a seguir, verifica se algum dos campos do ProcessoAdm foi alterado durante o update, se ocorreu a alteração, será criado um objeto no modelo Auditoria com os dados de registro e os campos alterados
+        """    
+        try:
+            objeto_original = self.get_object()
+            objeto_atualizado = form.instance
+
+            campos_alterados = []
+
+            if objeto_original.ativo != objeto_atualizado.ativo:
+                campos_alterados.append(f'| Campo: ativo ; Valor Antigo: {objeto_original.ativo} ; Valor Novo: {objeto_atualizado.ativo} |')
+            
+            if objeto_original.municipio != objeto_atualizado.municipio:
+                campos_alterados.append(f'| Campo: municipio ; Valor Antigo: {objeto_original.municipio} ; Valor Novo: {objeto_atualizado.municipio} |')
+
+            if objeto_original.uf != objeto_atualizado.uf:
+                campos_alterados.append(f'| Campo: uf ; Valor Antigo: {objeto_original.uf} ; Valor Novo: {objeto_atualizado.uf} |')
+
+            if objeto_original.data_inicial != objeto_atualizado.data_inicial:
+                campos_alterados.append(f'| Campo: data_inicial ; Valor Antigo: {objeto_original.data_inicial} ; Valor Novo: {objeto_atualizado.data_inicial} |')
+
+            if objeto_original.data_final != objeto_atualizado.data_final:
+                campos_alterados.append(f'| Campo: data_final ; Valor Antigo: {objeto_original.data_final} ; Valor Novo: {objeto_atualizado.data_final} |')
+
+            if objeto_original.data_div_ativa != objeto_atualizado.data_div_ativa:
+                campos_alterados.append(f'| Campo: data_div_ativa ; Valor Antigo: {objeto_original.data_div_ativa} ; Valor Novo: {objeto_atualizado.data_div_ativa} |')
+
+            if objeto_original.valor_atributo != objeto_atualizado.valor_atributo:
+                campos_alterados.append(f'| Campo: valor_atributo ; Valor Antigo: {objeto_original.valor_atributo} ; Valor Novo: {objeto_atualizado.valor_atributo} |')
+
+            if objeto_original.valor_multa != objeto_atualizado.valor_multa:
+                campos_alterados.append(f'| Campo: valor_multa ; Valor Antigo: {objeto_original.valor_multa} ; Valor Novo: {objeto_atualizado.valor_multa} |')
+
+            if objeto_original.valor_credito != objeto_atualizado.valor_credito:
+                campos_alterados.append(f'| Campo: valor_credito ; Valor Antigo: {objeto_original.valor_credito} ; Valor Novo: {objeto_atualizado.valor_credito} |')
+
+            if objeto_original.valor_atualizado != objeto_atualizado.valor_atualizado:
+                campos_alterados.append(f'| Campo: valor_atualizado ; Valor Antigo: {objeto_original.valor_atualizado} ; Valor Novo: {objeto_atualizado.valor_atualizado} |')
+
+            if objeto_original.data_valor_atualizado != objeto_atualizado.data_valor_atualizado:
+                campos_alterados.append(f'| Campo: data_valor_atualizado ; Valor Antigo: {objeto_original.data_valor_atualizado} ; Valor Novo: {objeto_atualizado.data_valor_atualizado} |')
+
+            if objeto_original.nome_contribuinte != objeto_atualizado.nome_contribuinte:
+                campos_alterados.append(f'| Campo: nome_contribuinte ; Valor Antigo: {objeto_original.nome_contribuinte} ; Valor Novo: {objeto_atualizado.nome_contribuinte} |')
+
+            if objeto_original.tipo_pessoa != objeto_atualizado.tipo_pessoa:
+                campos_alterados.append(f'| Campo: tipo_pessoa ; Valor Antigo: {objeto_original.tipo_pessoa} ; Valor Novo: {objeto_atualizado.tipo_pessoa} |')
+
+            if objeto_original.documento != objeto_atualizado.documento:
+                campos_alterados.append(f'| Campo: documento ; Valor Antigo: {objeto_original.documento} ; Valor Novo: {objeto_atualizado.documento} |')
+
+            if objeto_original.nome_fantasia != objeto_atualizado.nome_fantasia:
+                campos_alterados.append(f'| Campo: nome_fantasia ; Valor Antigo: {objeto_original.nome_fantasia} ; Valor Novo: {objeto_atualizado.nome_fantasia} |')
+
+            if objeto_original.email != objeto_atualizado.email:
+                campos_alterados.append(f'| Campo: email ; Valor Antigo: {objeto_original.email} ; Valor Novo: {objeto_atualizado.email} |')
+
+            if objeto_original.endereco != objeto_atualizado.endereco:
+                campos_alterados.append(f'| Campo: endereco ; Valor Antigo: {objeto_original.endereco} ; Valor Novo: {objeto_atualizado.endereco} |')
+
+            if objeto_original.complemento != objeto_atualizado.complemento:
+                campos_alterados.append(f'| Campo: complemento ; Valor Antigo: {objeto_original.complemento} ; Valor Novo: {objeto_atualizado.complemento} |')
+
+            if objeto_original.municipio_contribuinte != objeto_atualizado.municipio_contribuinte:
+                campos_alterados.append(f'| Campo: municipio_contribuinte ; Valor Antigo: {objeto_original.municipio_contribuinte} ; Valor Novo: {objeto_atualizado.municipio_contribuinte} |')
+                
+            if objeto_original.uf_contribuinte != objeto_atualizado.uf_contribuinte:
+                campos_alterados.append(f'| Campo: uf_contribuinte ; Valor Antigo: {objeto_original.uf_contribuinte} ; Valor Novo: {objeto_atualizado.uf_contribuinte} |')
+
+            if objeto_original.cep != objeto_atualizado.cep:
+                campos_alterados.append(f'| Campo: cep ; Valor Antigo: {objeto_original.cep} ; Valor Novo: {objeto_atualizado.cep} |')
+
+            if objeto_original.telefone != objeto_atualizado.telefone:
+                campos_alterados.append(f'| Campo: telefone ; Valor Antigo: {objeto_original.telefone} ; Valor Novo: {objeto_atualizado.telefone} |')
+
+            if objeto_original.celular != objeto_atualizado.celular:
+                campos_alterados.append(f'| Campo: celular ; Valor Antigo: {objeto_original.celular} ; Valor Novo: {objeto_atualizado.celular} |')
+
+            if campos_alterados:
+                # Registra a operação de alteração na auditoria
+                Auditoria.objects.create(
+                    usuario = self.request.user,
+                    objeto_id = self.object.id,
+                    tipo_objeto = 'processo administrativo',
+                    view = ProcessoAdmArquivadoUpdate,
+                    acao = 'update',
+                    processo = self.object.numero,
+                    campos_alterados = campos_alterados,              
+                )
+
+            return super().form_valid(form)
+
+        except Exception as erro:
+            logger.error(f'Erro ao editar objeto - View: ProcessoAdmArquivadoUpdate - Erro: {str(erro)}')
 
 class AndamentoAdmUpdate(UpdateView):
     model = AndamentoAdm
@@ -478,6 +599,35 @@ class ProcessoAdmDelete(DeleteView):
         context['dados_processo'] = ProcessoAdm.objects.filter(pk=processo_pk) # Filtra os dados do processo através da pk
         return context 
 
+class ProcessoAdmArquivadoDelete(DeleteView):
+    model = ProcessoAdm
+    template_name = 'processos/deletes/processo_adm_arquivado_delete.html'
+    success_url = reverse_lazy('proc-adm-arquivado-list')
+
+    def form_valid(self, form):        
+        try:
+            # Registre a operação de criação na auditoria
+            Auditoria.objects.create(
+                usuario = self.request.user,
+                objeto_id = self.object.id,
+                tipo_objeto = 'processo administrativo',
+                view = ProcessoAdmArquivadoDelete,
+                acao = 'delete',
+                processo = self.object.numero,
+                )
+            
+            return super().form_valid(form)
+        
+        except Exception as erro:
+            logger.error(f'Erro ao deletar objeto - View: ProcessoAdmArquivadoDelete - Erro: {str(erro)}')
+    
+    # Função para iterar com os dados do processo na view de delete processo
+    def get_context_data(self, **kwargs):
+        processo_pk = self.kwargs.get('pk') # Pega a PK do processo através da URL  
+
+        context = super().get_context_data(**kwargs)
+        context['dados_processo'] = ProcessoAdm.objects.filter(pk=processo_pk) # Filtra os dados do processo através da pk
+        return context 
 
 class AndamentoAdmDelete(DeleteView):
     model = AndamentoAdm
